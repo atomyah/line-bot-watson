89cf122fd57e47a7c12390c977b896ee5c31e406
Atom Yah <atom@yah>
2018-01-03 21:02:52 +0900

    P226

--- index.php
+++ index.php
@@ -135,7 +135,7 @@ class dbConnection {
       // 環境変数からデータベースへの接続情報を取得し
       $url = parse_url(getenv('DATABASE_URL'));
       // データソース
-      $dsn = sprinf('pgsql:host=%s;dbname=%s', $url['host'], substr(url['path'], 1));
+      $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr(url['path'], 1));
       // 接続を確立
       self::$db = new PDO($dsn, $url['user'], $url['pass']);
       // エラー時例外を投げるように設定
