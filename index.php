cb4e2265b68184198b3c36fb4846d86abc1dbb4e
Atom Yah <atom@yah>
2018-01-04 19:29:11 +0900

    setLastConversationData外で変数を表示

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ foreach ($events as $event) {
   // データベースに保存
   $conversationData = array('conversation_id' => $conversationId, 'dialog_node' => $dialogNode);
 //  replyTextMessage($bot, $event->getReplyToken(), $conversationData['conversation_id'] . 'と' . $conversationData['dialog_node']);  
-  setLastConversationData($event->getUserId(), $conversationData);
+  $Arry = setLastConversationData($event->getUserId(), $conversationData);
   replyTextMessage($bot, $event->getReplyToken(), $Arry[0] . 'と' . $Arry[1]);
   
   // Conversationからの返答を取得
